f0873a15092e1da6247d227c8d4ce50023421df3
Amirhh_04 <04.amirhh@gmail.com>
2023-10-25 18:29:20 +0330

    `p1_c4.m` Added(update)

--- Mechanics of Materials - Computer Problems/chapter-1/p1_c4.m
+++ Mechanics of Materials - Computer Problems/chapter-1/p1_c4.m
@@ -4,15 +4,25 @@ clc; clear;
 P = input("Force Applied [=> kips]: ");
 ultimate_load = input("Ultimate Load [=> kips]: ");
 
-alpha_range = (0:0.1:45);
-beta_range = (0:0.1:45);
+c = 0;
+alpha_range = cell(11, 1);
+beta_range = cell(11, 1);
 
-f_s = zeros(length(alpha_range), length(beta_range));
+for i = atand(0:0.1:1)
+    c = c + 1;
+    val = i.* (pi/180);
+    beta_range{c, 1} = val; alpha_range{c, 1} = val;
+end
 
-for i = 1:length(alpha_range)
-    for j = 1:length(beta_range)
-        alpha = alpha_range(i);
-        beta = beta_range(j);
+alpha_length = length(alpha_range);
+beta_length = length(beta_range);
+
+f_s = zeros(alpha_length, beta_length);
+
+for i = 1:alpha_length
+    for j = 1:beta_length
+        alpha = alpha_range{i, 1};
+        beta = beta_range{j, 1};
 
         force = P * ((15*sin(alpha) + 30 * cos(alpha)) / (15 * cos(beta) + 12 * sin(beta)));
 
@@ -20,18 +30,4 @@ for i = 1:length(alpha_range)
     end
 end
 
-% (b) محاسبه ماکزیمم فاکتور ایمنی برای هر مقدار α
-[max_safety_factors, max_beta_indices] = max(f_s);
-optimal_beta_values = beta_range(max_beta_indices);
-optimal_alpha_values = alpha_range;
-
-
-% (c) محاسبه مینیمم فاکتور ایمنی
-[min_safety_factor, min_safety_factor_idx] = min(f_s(:));
-[min_alpha_idx, min_beta_idx] = ind2sub(size(f_s), min_safety_factor_idx);
-
-
-
-% Display the results
-fprintf('(b) Maximum Safety Factor: %.4f for β = %.2f and α = %.2f in radians\n', max_safety_factors, optimal_beta_values, optimal_alpha_values);
-fprintf('(c) Minimum Safety Factor: %.4f for β = %.2f and α = %.2f in radians\n', min_safety_factor, beta_range(min_beta_idx), alpha_range(min_alpha_idx));
+disp(f_s);
