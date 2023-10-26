097a940a51b75a530d7ecb1d6027fd5398f0b332
Amirhh_04 <04.amirhh@gmail.com>
2023-10-26 14:47:15 +0330

    Update `p1_c1.m` - add SI and US unit (customary unit selection)

--- Mechanics of Materials - Computer Problems/chapter-1/p1_c1.m
+++ Mechanics of Materials - Computer Problems/chapter-1/p1_c1.m
@@ -1,14 +1,25 @@
 %%%%%%%%%% P1-C1 %%%%%%%%%%
 clc; clear;
 
+customary_unit = input("Choose unit system ['SI' || 'US']: ");
 tElements = input("Total Elements : ");
+
+if ~ (customary_unit == "US") && ~ (customary_unit == "SI"); disp("Please enter the correct customary units!"); return; end;
 if ~ isnumeric(tElements) || ~ isscalar(tElements) || ~(tElements > 0); disp("Please enter a number!"); return; end;
+if customary_unit == "SI"
+    unit_si = true;
+    unitData = struct('diameter', 'm', 'load', 'N');
+else
+    unit_si = false;
+    unitData = struct('diameter', 'in', 'load', 'kips');
+end
 
 data = cell(tElements, 1);
 
 for c = 1:tElements
-    diameter = input("Diameter (E:" + c + ") [=> m]: ");
-    load = input("Load Applied (E:" + c + ") [=> N]: ");
+    e_l = num2str(c);
+    diameter = input(['Diameter (E:'  e_l  ') [=> ' unitData.diameter ']: ']);
+    load = input(['Load Applied (E:'  e_l  ') [=> ' unitData.load ']: ']);
 
     data{c, 1} = struct('diameter', diameter, 'load', load);
 
@@ -20,5 +31,13 @@ for c = 1:tElements
 end
 
 for c = 1:tElements
-    fprintf('\nElement %d \n    Force: %.2f (N)\n    Area: %.4e (M²)\n    Average Stress: %.4f (Pa)\n', c, data{c}.load, data{c}.area, data{c}.averageStress);
+    if unit_si == true
+        fprintf('\n ------ Element %d ------\n   Force: %.2f (N)\n   Area: %.4e (m²)\n   Average Stress: %.4f (MPa)\n', c, data{c}.load, data{c}.area, pascalsToMegapascals(data{c}.averageStress));
+    else
+        fprintf('\n ------ Element %d ------\n   Force: %.2f (kips)\n   Area: %.4e (in²)\n   Average Stress: %.4f (ksi)\n', c, data{c}.load, data{c}.area, data{c}.averageStress);
+    end
+end
+
+function megapascals = pascalsToMegapascals(pascals)
+    megapascals = pascals / 1e6;
 end
