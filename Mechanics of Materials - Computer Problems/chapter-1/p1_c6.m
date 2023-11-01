e087e0d8f19c4102dd3e2fd55d200908fe75a6eb
Amirhh_04 <04.amirhh@gmail.com>
2023-11-01 12:47:04 +0330

    `p1_c6` Updated - added initial code

---
+++ Mechanics of Materials - Computer Problems/chapter-1/p1_c6.m
@@ -0,0 +1,52 @@
+%%%%%%%%%% P1-C6 %%%%%%%%%%
+clc; clear;
+
+f_o_s = input("Factor Of Safety (F.S.): ");
+u_s_s = input("Ultimate Shearing Stress [=> MPa]: ");
+u_n_s = input("Ultimate Normal Stress [=> MPa]: ");
+width_bd = input("Enter the width BD: [=> m]: ");
+height_bd = input("Enter the height BD: [=> m]: ");
+
+baseData = struct('label_1', 'AB', 'label_2', 'BC', 'pin_1', 'A', 'pin_2', 'B');
+data = cell(3, 1);
+
+for c = 1:3
+    if c < 3
+        label_name = ['label_' num2str(c)];
+        distance = input(['Enter the distance ' baseData.(label_name) ' [=> m]: ']);
+    
+        pin_name = ['pin_' num2str(c)];
+        diameter = input(['Enter the pin diameter ' baseData.(pin_name) ' [=> m]: ']);
+    
+        dis_name = ['distance_' baseData.(label_name)];
+        pin_name = ['pinDiameter_' baseData.(pin_name)];
+    
+        baseData.(dis_name) = distance;
+        baseData.(pin_name) = diameter;
+    
+        data{c, 1} = struct('diameter', diameter);
+    
+        if c == 2
+            data{1, 1}.p = (baseData.distance_AB / baseData.distance_BC);
+            data{2, 1}.p = (baseData.distance_AB / (baseData.distance_AB + baseData.distance_BC));
+        end
+    else
+        data{3, 1}.p = (baseData.distance_AB / (baseData.distance_AB + baseData.distance_BC));
+    end
+end
+
+for c = 1:3
+    if c < 3
+        area = (pi / 4) * (data{c, 1}.diameter)^2;
+        f = ((2) * (u_s_s) * (area)) / (f_o_s);
+    else
+        area = width_bd * height_bd;
+        f = ((2) * (u_n_s) * (area)) / (f_o_s);
+    end
+
+    p =  data{c, 1}.p * f;
+
+    data{c, 1}.area = area;
+    data{c, 1}.f = f;
+    data{c, 1}.p = p;
+end
