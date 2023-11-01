371f262fb34384217fd329fb600d6082c960a677
Amirhh_04 <04.amirhh@gmail.com>
2023-11-02 01:32:07 +0330

    `p2_c5` Updated (label-name)

--- Mechanics of Materials - Computer Problems/chapter-2/p2_c5.m
+++ Mechanics of Materials - Computer Problems/chapter-2/p2_c5.m
@@ -1,18 +1,18 @@
 %%%%%%%%% P2-C5 %%%%%%%%%%
 clc; clear;
 
-radius_hole = input("Radius Of The Hole (Exa: [.125, .25]) [=> in]: ");
-width_bar = input("Width Of The Bar [=> in]: ");
-thickness = input("Thickness Of The Bar [=> in]: ");
-sigma_all = input("S(avg) [=> ksi]: ");
+hole_radius = input("Radius Of The Hole (Exa: [.125, .25]) [=> in]: ");
+bar_width = input("Width Of The Bar [=> in]: ");
+bar_thickness = input("Thickness Of The Bar [=> in]: ");
+allowable_stress = input("Allowable Stress (avg) [=> ksi]: ");
 
-for c = 1:length(radius_hole)
-    r_h = radius_hole(c);
+for c = 1:length(hole_radius)
+    hole_r = hole_radius(c);
 
-    RD = 2.0*(r_h / width_bar);
+    RD = 2.0*(hole_r / bar_width);
     K = 3.00 - 3.13*(RD) + 3.66*((RD)^2) - 1.53*((RD)^3);
-    sigma_avg = sigma_all / K;
-    p_all = sigma_avg * (width_bar - 2.0*(r_h))*thickness;
+    sigma_avg = allowable_stress / K;
+    p_all = sigma_avg * (bar_width - 2.0*(hole_r))*bar_thickness;
 
-    fprintf('\n ------------------------\n   Radius : %.4f (in)\n   Allowable Load: %.4f (kips)', r_h, p_all);
+    fprintf('\n ------------------------\n   Hole Radius : %.4f (in)\n   Allowable Load: %.4f (kips)', hole_r, p_all);
 end
