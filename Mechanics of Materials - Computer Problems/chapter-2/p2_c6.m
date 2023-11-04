f49421ecfc71c73efa12e7994b43ea1055c96ade
Amirhh_04 <04.amirhh@gmail.com>
2023-11-04 12:13:13 +0330

    `p2_c6` Updated - `p2_c6 Completed

--- Mechanics of Materials - Computer Problems/chapter-2/p2_c6.m
+++ Mechanics of Materials - Computer Problems/chapter-2/p2_c6.m
@@ -1,28 +1,32 @@
 %%%%%%%%% P2-C6 %%%%%%%%%%
 clc; clear;
 
-%n_nums = input("Enter n [=> 1,4, ...]: ");
-n_nums = [6, 12, 60];
-P = 1000;
-L = 10;
-c = 1;
-E = 200e9;
+n_nums = input(" Enter n [=> 1,4, ...]: "); % n_nums = [6, 12, 60]
+P = input(" Enter P [=> N]: "); % P = 1000;
+L = input(" Enter L [=> m]: "); % L = 10;
+c = input(" Enter c [=> m]: "); % c = 1;
+E = input(" Enter E [=> GPa]: "); % E = 200e9;
 
-data = cell(length(n_nums), 1);
-delta = 0;
-delta_exact = (P * L) / (2.0*pi*(c^2)*E);
+delta_exact = (P * L) / (2 * pi * c^2 * E);
+delta_approx = cell(length(n_nums), 1);
+percent_errors = cell(length(n_nums), 1);
 
-for c = 1:length(n_nums)
-    n = n_nums(c);
+for i = 1:length(n_nums)
+    n = n_nums(i);
+
+    delta_approx{i, 1} = approximate_elongation(P, L, c, E, n);
+    percent_errors{i, 1} = 100 * (delta_approx{i, 1} - delta_exact) / delta_exact;
+
+    fprintf('\n ------------------------\n   n: %d \n   Approximate: %.4e \n   Exact: %.4e \n   Percentage Error: %f%%', n, delta_approx{i, 1}, delta_exact, percent_errors{i, 1});
+end
+
+function delta_approx = approximate_elongation(P, L, c, E, n)
+    delta_approx = 0;
+    
     for i = 1:n
-        len = (i + 0.5)*(L / n);
-        r = (2*c) - c*(len / L);
-        area = pi * (r^2);
-
-        delta = delta + P * (L / n) / (area * E);
+        len = (i - 0.5) * (L / n);
+        r = 2 * c - c * (len / L);
+        a = pi * r^2;
+        delta_approx = delta_approx + (P * (L / n)) / (a * E);
     end
-
-    percent = 100*(delta - delta_exact) / delta_exact;
-
-    data{c, 1} = struct('delta', delta, 'percent', percent, 'delta_exact', delta_exact);
 end
